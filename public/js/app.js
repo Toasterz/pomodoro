334a8760347bbddbe5bd17c5091936fc1fbba6b3
Jacob Greenberg <jgreenberg45@yahoo.com>
2016-06-20 20:25:01 -0400

    Got seconds working on the clock shoutout to hashes

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-console.log("My JS works");
+$(document).ready(function(){
+  var start =$('#start');
+  var minutes = $('#mins');
+  var secs = $('#secs');
+
+  start.on('click', startCountdown);
+
+function startCountdown(){
+  setInterval(function(){
+    var secondsVal = +secs.text();
+    if(secondsVal ===0)
+    {
+      secs.text(59);
+    }
+    else
+    {
+      if(secondsVal <= 10)
+      {
+        secs.text("0" + (secondsVal-1));
+      }
+      else {
+        secs.text(secondsVal-1);
+      }
+    }
+    }, 1000);
+  }
+});
